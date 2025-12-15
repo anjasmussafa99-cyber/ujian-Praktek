2897299f25308e03bb4ca4140159ec097b12433a
anjasmussafa99-cyber <anjasmussafa99@gmail.com>
2025-12-15 13:38:32 +0700

    Add files via upload

---
+++ coding 2.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+int main() {
+	int p;
+	cout << "Masukkan Bilangan:";
+	cin >> p;
+
+	if (p = 2 == 0) {
+		cout << "Ganjil" << endl;
+	}
+	else {
+		cout << "Genap" << endl;
+	}
+	if (p > 10) {
+		cout << "Di Atas 10" << endl;
+	}
+	else {
+		cout << "Di Bawah 10" << endl;
+	}
+	return 0;
+}
+
+
+
+
